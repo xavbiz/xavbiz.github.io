53660428952d1d8489bf30ddfb3538bf80b883f2
x_x@home <mlegrixdelasalle@gmail.com>
2022-03-23 09:21:31 +0000

    fix docs

--- docs/templates/__react_static_root__/src/pages/Contact.fca1562e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{91:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a),c=t(54);e.default=function(){return u.a.createElement(c.a,null)}}}]);
